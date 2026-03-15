4032ba0806265bd7dab19b6ab2ef34ab3df81db5
Harsh Mhatre <your-email@gmail.com>
2026-03-15 16:43:54 +0530

    aws ec2 deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 await connectDB()
 
 
